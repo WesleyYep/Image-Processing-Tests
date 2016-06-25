ae28adc7ddfb307407d42ae04f0eb5ac0a0c0f7c
WesleyYep <wesleyyep@hotmail.com>
2016-06-25 15:15:04 +1200

    works now

--- src/TestColourDetection.java
+++ src/TestColourDetection.java
@@ -1,3 +1,11 @@
+import java.awt.FlowLayout;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map.Entry;
+
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 
@@ -18,30 +26,36 @@ public class TestColourDetection {
         System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
     }
     
-    public static void setupSlider(RangeSlider slider, int majorSpacing, MyFrame frame) {
+    public static void setupSlider(RangeSlider slider, int majorSpacing, JFrame frame) {
     	slider.setMajorTickSpacing(majorSpacing);
     	slider.setMinorTickSpacing(10);
     	slider.setPaintTicks(true);
         slider.setPaintLabels(true);
-        slider.setValue(120);
+        slider.setValue(140);
         slider.setUpperValue(255);
-        frame.add(slider);
+        frame.getContentPane().add(slider);
     }
 	
     public static void main ( String[] argv) {
 	    VideoCapture cap = new VideoCapture(0); //capture the video from webcam
-	    MyFrame frame = new MyFrame();
-        frame.setVisible(true);
-        MyFrame frame2 = new MyFrame();
-        frame2.setVisible(true);
+	    MyFrame thresholdFrame = new MyFrame();
+        thresholdFrame.setVisible(true);
+        MyFrame imgFrame = new MyFrame();
+        imgFrame.setVisible(true);
+        JFrame controlsFrame = new JFrame();
+        controlsFrame.setVisible(true);
+        controlsFrame.getContentPane().setLayout(new FlowLayout());
         RangeSlider hSlider = new RangeSlider(0,180);
         RangeSlider sSlider = new RangeSlider(0,255);
         RangeSlider vSlider = new RangeSlider(0,255);
-        setupSlider(hSlider, 60, frame);
-        setupSlider(sSlider, 50, frame);
-        setupSlider(vSlider, 50, frame);
+        setupSlider(hSlider, 60, controlsFrame);
+        setupSlider(sSlider, 50, controlsFrame);
+        setupSlider(vSlider, 50, controlsFrame);
         hSlider.setValue(0);
-        hSlider.setUpperValue(5);
+        hSlider.setUpperValue(10);
+//        sSlider.setValue(160);
+//        vSlider.setValue(140);
+        controlsFrame.pack();
 
         if(!cap.isOpened()) {
             System.out.println("cannot open camera");
@@ -82,10 +96,69 @@ public class TestColourDetection {
 	        Imgproc.erode(imgThresholded, imgThresholded, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(5, 5)) );
 	
 	        //see if we can find blobs based on contours
-	        MatOfKeyPoint keyPoints = new MatOfKeyPoint();
-	        FeatureDetector blobDetector = FeatureDetector.create(FeatureDetector.SIMPLEBLOB);
-	        blobDetector.detect(imgThresholded, keyPoints);
-	        System.out.println("blob count: " + keyPoints.size());
+//	        MatOfKeyPoint keyPoints = new MatOfKeyPoint();
+//	        FeatureDetector blobDetector = FeatureDetector.create(FeatureDetector.SIMPLEBLOB);
+//	        blobDetector.detect(imgThresholded, keyPoints);
+	        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+	        Mat hierarchy = new Mat();
+	        Imgproc.findContours(imgThresholded, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);;
+//	        System.out.println("blob count: " + contours.size());
+	        if (contours.size() >= 3) {
+	        	List<Entry<Point, Double>> markers = new ArrayList<Entry<Point, Double>>();
+	        	for (int i = 0; i < contours.size(); i++) {
+	        		Moments moments = Imgproc.moments(contours.get(i));
+	        		double dM01 = moments.m01;
+	    	        double dM10 = moments.m10;
+	    	        double dArea = moments.m00;
+	    	        if (dArea > 50) {
+	    	        	//calculate the position of the marker
+	    	        	int posX = (int) (dM10 / dArea);
+	    	        	int posY = (int) (dM01 / dArea);
+	    	        	markers.add(new AbstractMap.SimpleEntry<Point,Double>(new Point(posX, posY), dArea));
+	    	//        	System.out.println("BLOB " + i + ": x: " + posX + " - y: " + posY + " area: " + dArea);
+	    	        } else {
+	    	//        	System.out.println("area is only " + dArea);
+	    	        }
+	        	}
+	        	double min = 999999998;
+	        	int firstMarker = 0;  //top-left marker
+	        	List<Point> actualMarkers = new ArrayList<Point>();
+	        	for (int i = 0; i < markers.size(); i++) {
+	        		double first = 0;
+	        		double difference = 999999999;
+	        		for (int j = 0; j < markers.size(); j++) {
+	        			if (i != j) { 
+	        				double ratio = distance(markers.get(i).getKey(), markers.get(j).getKey())/(markers.get(i).getValue());
+	        				if (ratio < 0.2) {
+	        					if (first == 0) {
+	        						first = ratio;
+	        					} else if (Math.abs(ratio - first) < difference){
+	        						difference = Math.abs(ratio - first);
+	        						actualMarkers.add(markers.get(i).getKey());
+	        						break;
+	        					}
+	        				}
+        				}
+	        		}
+	        		if (difference < min) {
+	        			firstMarker = actualMarkers.size() - 1;
+	        			min = difference;
+	        		}
+	        	}
+	        	if (actualMarkers.size() == 3) {
+//	        		System.out.println("FIRST: " + firstMarker);
+//	        		Imgproc.circle(imgOriginal, actualMarkers.get(firstMarker), 2, new Scalar(0,255,0));
+	        		double totalX = 0, totalY =0;
+	        		for (Point p : actualMarkers) {
+	        			totalX += p.x;
+	        			totalY += p.y;
+	        		}
+	        		double avX = totalX / 3;
+	        		double avY = totalY / 3;
+		        	System.out.println("AVERAGE: x: " + avX + " - y: " + avY);
+	        		Imgproc.circle(imgOriginal, new Point(avX, avY), 2, new Scalar(0,255,0),5);
+	        	}
+	        }
 	        
 	        //Calculate the moments of the thresholded image
 	        Moments oMoments = Imgproc.moments(imgThresholded);
@@ -99,21 +172,25 @@ public class TestColourDetection {
 	        	//calculate the position of the ball
 	        	int posX = (int) (dM10 / dArea);
 	        	int posY = (int) (dM01 / dArea);       
-	        	System.out.println("x: " + posX + " - y: " + posY);
+	        	System.out.println("OVERALL: x: " + posX + " - y: " + posY);
 		        
 	        	if (iLastX >= 0 && iLastY >= 0 && posX >= 0 && posY >= 0) {
 	        		//Draw a red line from the previous point to the current point
-	        		Imgproc.line(imgLines, new Point(posX, posY), new Point(iLastX, iLastY), new Scalar(0,0,255), 2);
+//	        		Imgproc.line(imgLines, new Point(posX, posY), new Point(iLastX, iLastY), new Scalar(0,0,255), 2);
 	        	}
 	
 	        	iLastX = posX;
 	        	iLastY = posY;
 	        }
-        	frame.render(imgThresholded);//show the thresholded image
-        	Core.add(imgOriginal,imgLines, imgOriginal);
-        	frame2.render(imgOriginal); //show the original image
+        	thresholdFrame.render(imgThresholded);//show the thresholded image
+      //  	Core.add(imgOriginal,imgLines, imgOriginal);
+        	imgFrame.render(imgOriginal); //show the original image
 	    }
 	    return;
+	}
+
+	private static double distance(Point point, Point point2) {
+		return Math.sqrt(Math.pow(point.x-point2.x, 2) + Math.pow(point.y-point2.y, 2));
 	}
    
 }
